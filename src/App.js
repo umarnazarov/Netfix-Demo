9c5e12b93d2e70f39ab42839580b58e85de69ea2
Umar Nazarov <1nazarovumar@gmail.com>
2021-07-06 17:13:35 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -9,19 +9,23 @@ import PrivateSignUpPage from './components/PrivateSignUpPage'
 import PrivateUserPage from './components/PrivateUserPage'
 import PrivateFilmRoute from './components/PrivateFilmRoute'
 import Films from "./components/Films";
-
+import PrivateMoviePage from "./components/PrivateMoviePage"
+import MoviesProvider from "./context/MoviesContext"
 
 function App() {
   return (
     <AuthProvider>
-      <Switch>
-        <Redirect exact from="/" to="/home" />
-        <Route path='/home' exact component={Main} />
-        <PrivateLoginPage path='/login' exact component={Login} />
-        <PrivateSignUpPage path='/signup' exact component={SignUp} />
-        <PrivateUserPage path='/home/profile' exact component={Profile} />
-        <PrivateFilmRoute path='/home/films' exact component={Films} />
-      </Switch>
+      <MoviesProvider>
+        <Switch>
+          <Redirect exact from="/" to="/home" />
+          <Route path='/home' exact component={Main} />
+          <PrivateLoginPage path='/login' exact component={Login} />
+          <PrivateSignUpPage path='/signup' exact component={SignUp} />
+          <PrivateFilmRoute path='/home/films' exact component={Films} />
+          <PrivateUserPage path='/home/profile' exact component={Profile} />
+          <PrivateMoviePage />
+        </Switch>
+      </MoviesProvider>
     </AuthProvider>
 
   );
